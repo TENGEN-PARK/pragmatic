fc45353c551661db76c874798d1fd7011ca2b2c1
PARKSUNGWOO <tjddn7500@gmail.com>
2024-07-26 11:05:08 +0900

    django course 29 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accountapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
@@ -146,3 +147,7 @@ LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world'),
 
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
